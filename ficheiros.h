10f050f69a2a1c53e7026a19f5b2985ab8a0b9f4
massas0 <34848478+massas0@users.noreply.github.com>
2021-12-17 15:29:45 +0000

    Revert "Upload Projeto v2"
    This reverts commit 45771a06931bf70fb29019e2c46c95253b6aea8f.

--- ficheiros.h
+++
@@ -1,9 +0,0 @@
-#ifndef FICHEIROS_H_INCLUDED
-#define FICHEIROS_H_INCLUDED
-
-#include "dados.h"
-
-extern void log_vacina(char[MAX_NOME], int, t_membro[]);
-extern void gravar_lembros( t_membro*, int );
-
-#endif // FICHEIROS_H_INCLUDED
